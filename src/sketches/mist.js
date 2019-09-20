f4ec0781ef0f4d3c3fe9431ddf33605efbeaa63b
Felicia Chang <felicia.chang@yale.edu>
2019-09-19 23:08:47 -0400

    positioning

--- src/sketches/mist.js
+++ src/sketches/mist.js
@@ -45,16 +45,16 @@ export default function mist(p) {
     p.text('01', 100, 250, 170, 170);
 
     p.fill(200, p.mouseX/10, p.mouseX/10, 100);
-    p.rect(window.innerWidth - 800, 100, d1, 850);
+    p.rect(window.innerWidth - 650, 100, d1, 850);
     p.fill(0);
     p.textSize(window.innerWidth/70);
     p.textLeading(window.innerHeight/35);
-    p.text(story, window.innerWidth - 750, 150);
-    p.text(story2, window.innerWidth - 750, 420);
-    p.text(story3, window.innerWidth - 750, 690);
+    p.text(story, window.innerWidth - 600, 150);
+    p.text(story2, window.innerWidth - 600, 350);
+    p.text(story3, window.innerWidth - 600, 550);
     p.textLeading(window.innerHeight/50);
     p.text('move mouse to interact', window.innerWidth - 100, 60, 40, 640);
-        p.text('click for next page', window.innerWidth - 100, (window.innerHeight/4)*3+100, 40, 640);
+    p.text('click for next page', window.innerWidth - 100, (window.innerHeight/4)*3+100, 40, 640);
 
     p.fill(196, 56, 43, 10);
     p.noStroke();
