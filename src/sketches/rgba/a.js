8ff3502681dca2446665c5cd765474a43b94b2d6
Felicia Chang <felicia.chang@yale.edu>
2020-11-29 23:48:53 -0500

    blue and yellow

--- src/sketches/rgba/a.js
+++ src/sketches/rgba/a.js
@@ -1,27 +1,63 @@
 import * as p5 from "p5";
 import "p5/lib/addons/p5.sound";
 import "p5/lib/addons/p5.dom";
-import Elvn from "../assets/11pm.mp3";
-import BK from "../assets/black-kosa.mp3";
-import tambora from "../assets/tambora.jpg";
-import painting from "../assets/painting.jpg";
-import photo2 from "../assets/photo2.jpg";
+import rgby from "./assets/rbgy_ambience.mp3";
+import yellow from "./assets/yellow/yellow_voices.mp3";
+import yellowhead from "./assets/yellow/yellowhead.png";
+import honeycombs from "./assets/yellow/honeycombs.png";
+import smiley from "./assets/yellow/smiley.png";
 
 export default function A(p) {
-  let song, song2, analyzer, img, mic, fft, you, pho;
+  let particles = [];
+  let song, rms, song2, analyzer, img, img2, img3, mic, fft, you, pho, voice;
+
+  // this class describes the properties of a single particle.
+  class Particle {
+    // setting the co-ordinates, radius and the
+    // speed of a particle in both the co-ordinates axes.
+    constructor() {
+      this.x = p.random(0, window.innerWidth);
+      this.y = p.random(0, window.innerHeight);
+      this.r = p.random(10, 100);
+      this.xSpeed = p.random(-2, 2);
+      this.ySpeed = p.random(-1, 1.5);
+    }
+
+    // creation of a particle.
+    createParticle(img) {
+      p.image(img, this.x, this.y, this.r, this.r);
+    }
+
+    // setting the particle in motion.
+    moveParticle() {
+      if (this.x < 0 || this.x > window.innerWidth) this.xSpeed *= -1;
+      if (this.y < 0 || this.y > window.innerHeight) this.ySpeed *= -1;
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+    }
+
+    moveParticleToCenter() {
+      if (this.x < 0 || this.x > window.innerWidth) this.xSpeed *= -5;
+      if (this.y < 0 || this.y > window.innerHeight) this.ySpeed *= -5;
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+    }
+  }
 
   p.preload = () => {
-    song = p.loadSound(Elvn);
-    song2 = p.loadSound(BK);
-    img = p.loadImage(tambora);
-    you = p.loadImage(painting);
-    pho = p.loadImage(photo2);
+    song = p.loadSound(rgby);
+    song2 = p.loadSound(yellow);
+    img = p.loadImage(yellowhead);
+    img2 = p.loadImage(honeycombs);
+    img3 = p.loadImage(smiley);
   };
 
   p.setup = () => {
-    p.background(img);
     p.createCanvas(window.innerWidth, window.innerHeight);
-    // p.background(255, 0, 0);
+
+    for (let i = 0; i < window.innerHeight / 50; i++) {
+      particles.push(new Particle());
+    }
 
     mic = new p5.AudioIn();
     mic.start();
@@ -30,21 +66,51 @@ export default function A(p) {
 
     // create a new Amplitude analyzer
     analyzer = new p5.Amplitude();
+    voice = new p5.Amplitude();
 
     // Patch the input to an volume analyzer
     analyzer.setInput(song);
+    voice.setInput(song2);
 
     song.play();
     song2.play();
   };
 
   p.draw = () => {
+    let rms = analyzer.getLevel();
+    let voicelevel = voice.getLevel();
+    p.background(200, 200, 0, 1);
     // p.image(you, Math.floor(rms * window.innerWidth), 100, 200, 500);
     // // // Draw an ellipse with size based on volume
-    for (let i = -100; i < window.innerWidth + 100; i = i + 100) {
-      for (let j = -100; j < window.innerHeight + 100; j = j + 100) {
-        p.text("A", i, j);
+
+    // for (let i = 0; i < window.innerWidth; i = i + 100) {
+    //   for (let j = 0; j < window.innerHeight; j = j + 100) {
+    //     p.image(
+    //       img2,
+    //       i,
+    //       j,
+    //       i / 10 - voicelevel * window.innerHeight,
+    //       j / 10 - voicelevel * window.innerHeight
+    //     );
+    //   }
+    // }
+
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].createParticle(img3);
+      particles[i].moveParticle();
+    }
+
+    if (rms > 0.1) {
+      for (let i = 0; i < particles.length; i++) {
+        particles[i].createParticle(img);
+        particles[i].moveParticleToCenter();
       }
     }
+
+    // for (let i = -100; i < window.innerWidth + 100; i = i + 100) {
+    //   for (let j = -100; j < window.innerHeight + 100; j = j + 100) {
+    //     p.text("B", i, j);
+    //   }
+    // }
   };
 }
--- src/sketches/rgba/r.js
+++ src/sketches/rgba/r.js
@@ -1,27 +1,59 @@
 import * as p5 from "p5";
 import "p5/lib/addons/p5.sound";
 import "p5/lib/addons/p5.dom";
-import Elvn from "../assets/11pm.mp3";
-import BK from "../assets/black-kosa.mp3";
-import tambora from "../assets/tambora.jpg";
-import painting from "../assets/painting.jpg";
-import photo2 from "../assets/photo2.jpg";
+import rgby from "./assets/rbgy_ambience.mp3";
+import facetrash from "./assets/blue/bluetrashcropped.png";
+import gloves from "./assets/blue/gloves.png";
 
-export default function R(p) {
-  let song, song2, analyzer, img, mic, fft, you, pho;
+export default function B(p) {
+  let particles = [];
+  let song, rms, song2, analyzer, img, img2, mic, fft, you, pho;
+
+  // this class describes the properties of a single particle.
+  class Particle {
+    // setting the co-ordinates, radius and the
+    // speed of a particle in both the co-ordinates axes.
+    constructor() {
+      this.x = p.random(0, window.innerWidth);
+      this.y = p.random(0, window.innerHeight);
+      this.r = p.random(10, 100);
+      this.xSpeed = p.random(-2, 2);
+      this.ySpeed = p.random(-1, 1.5);
+    }
+
+    // creation of a particle.
+    createParticle(img) {
+      p.image(img, this.x, this.y, this.r, this.r);
+    }
+
+    // setting the particle in motion.
+    moveParticle() {
+      if (this.x < 0 || this.x > window.innerWidth) this.xSpeed *= -1;
+      if (this.y < 0 || this.y > window.innerHeight) this.ySpeed *= -1;
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+    }
+
+    moveParticleToCenter() {
+      if (this.x < 0 || this.x > window.innerWidth) this.xSpeed *= -5;
+      if (this.y < 0 || this.y > window.innerHeight) this.ySpeed *= -5;
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+    }
+  }
 
   p.preload = () => {
-    song = p.loadSound(Elvn);
-    song2 = p.loadSound(BK);
-    img = p.loadImage(tambora);
-    you = p.loadImage(painting);
-    pho = p.loadImage(photo2);
+    song = p.loadSound(rgby);
+    img = p.loadImage(facetrash);
+    img2 = p.loadImage(gloves);
   };
 
   p.setup = () => {
-    p.background(img);
     p.createCanvas(window.innerWidth, window.innerHeight);
-    // p.background(255, 0, 0);
+
+    for (let i = 0; i < window.innerHeight / 50; i++) {
+      particles.push(new Particle());
+    }
 
     mic = new p5.AudioIn();
     mic.start();
@@ -35,16 +67,29 @@ export default function R(p) {
     analyzer.setInput(song);
 
     song.play();
-    song2.play();
   };
 
   p.draw = () => {
+    let rms = analyzer.getLevel();
+    p.background(0, 0, 200, 1);
     // p.image(you, Math.floor(rms * window.innerWidth), 100, 200, 500);
     // // // Draw an ellipse with size based on volume
-    for (let i = -100; i < window.innerWidth + 100; i = i + 100) {
-      for (let j = -100; j < window.innerHeight + 100; j = j + 100) {
-        p.text("R", i, j);
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].createParticle(img);
+      particles[i].moveParticle();
+    }
+
+    if (rms > 0.1) {
+      for (let i = 0; i < particles.length; i++) {
+        particles[i].createParticle(img2);
+        particles[i].moveParticleToCenter();
       }
     }
+
+    // for (let i = -100; i < window.innerWidth + 100; i = i + 100) {
+    //   for (let j = -100; j < window.innerHeight + 100; j = j + 100) {
+    //     p.text("B", i, j);
+    //   }
+    // }
   };
 }
